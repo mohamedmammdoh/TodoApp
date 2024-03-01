ada6c8ff2d3b6fe8883bf79d4971f0a8294d2f5e
mohamedmammdoh <mohamedmammdohharby@gmail.com>
2024-03-01 20:50:54 +0200

    provide add note cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:todoapp/constants.dart';
+import 'package:todoapp/cubit/addnote_cubit/addnote_cubit.dart';
 import 'package:todoapp/models/Notemodel.dart';
 import 'package:todoapp/views/NotesViews.dart';
 
@@ -21,13 +23,20 @@ class TodoApp extends StatefulWidget {
 class _TodoAppState extends State<TodoApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        fontFamily: 'Poppins',
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AddnoteCubit(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          fontFamily: 'Poppins',
+        ),
+        home: const NotesView(),
       ),
-      home: const NotesView(),
     );
   }
 }
