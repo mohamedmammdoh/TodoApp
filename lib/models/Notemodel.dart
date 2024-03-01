801e057ff15dcec7e7fb442d9cfb38e33b2e2a73
mohamedmammdoh <mohamedmammdohharby@gmail.com>
2024-03-01 09:29:16 +0200

    create generate file

--- lib/models/Notemodel.dart
+++ lib/models/Notemodel.dart
@@ -1,7 +1,15 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+part 'Notemodel.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(1)
   final String title;
+  @HiveField(2)
   final String subtitle;
+  @HiveField(3)
   final String date;
+  @HiveField(4)
   final int color;
 
   NoteModel(
